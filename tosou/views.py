e1c7641609d27c5e288099e90ee7a29dd562401e
shion-boss <sss.tl.ges.sss@gmail.com>
2020-06-14 22:12:07 +0900

    Update views.py

--- tosou/views.py
+++ tosou/views.py
@@ -116,7 +116,8 @@ def callback_view(request):
                     afi_code='{:0=6}'.format(int(code.num))
                     code.num+=1
                     code.save()
-                    user_meta(username=name,top=top,afi_code=afi_code,uid=line_user_id).save()
+                    meta=user_meta(username=str(name),top=str(top),afi_code=int(afi_code),uid=str(line_user_id))
+                    meta.save()
                 else:
                     #ブロック解除のユーザー
                     pass
@@ -134,7 +135,8 @@ def callback_view(request):
                 afi_code='{:0=6}'.format(int(code.num))
                 code.num+=1
                 code.save()
-                user_meta(username=str(name),top=top,afi_code=int(afi_code),uid=str(line_user_id)).save()
+                meta=user_meta(username=str(name),top=str(top),afi_code=int(afi_code),uid=str(line_user_id))
+                meta.save()
 
             welcome='大槻塗装公式LINEをご登録いただきありがとうございます。\n\n現金負担0円塗装をより多くの方々にお届けするために、\nお仕事をご紹介してくださった方、お仕事を依頼してくださった方へ、感謝の気持ちを込めて、紹介特典のプレゼント企画を行うことにしました。\n'+name+'様限定の紹介コードが「'+str(afi_code)+'」です。\n紹介特典のカタログや現金負担0円塗装の詳細は、下記URLにてご覧ください。'
             line_bot_api.push_message(line_user_id, TextSendMessage(text=welcome))
