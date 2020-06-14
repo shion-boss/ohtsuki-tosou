76069f07393372d48ab1687f5cca436c4e496782
shion-boss <sss.tl.ges.sss@gmail.com>
2020-06-14 15:43:39 +0900

    Update views.py

--- tosou/views.py
+++ tosou/views.py
@@ -67,7 +67,7 @@ def callback_view(request):
                     try:
                         line_bot_api.reply_message(
                             event.reply_token,
-                            TextSendMessage(text=str(event['source']["userId"]))
+                            TextSendMessage(text=str(event))
                         )
                     except LineBotApiError as e:
                         print(e.status_code)
