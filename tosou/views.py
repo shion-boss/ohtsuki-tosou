ecc23625b2a10b5531be7873a62d777eecd66899
shion-boss <sss.tl.ges.sss@gmail.com>
2020-06-14 14:56:22 +0900

    Update views.py

--- tosou/views.py
+++ tosou/views.py
@@ -67,7 +67,7 @@ def callback_view(request):
                     try:
                         line_bot_api.reply_message(
                             event.reply_token,
-                            TextSendMessage(text=event.source.userId)
+                            TextSendMessage(text=str(events))
                         )
                     except LineBotApiError as e:
                         print(e.status_code)
@@ -78,7 +78,7 @@ def callback_view(request):
                 try:
                     line_bot_api.reply_message(
                         event.replyToken,
-                        TextSendMessage(text=event.source.userId)
+                        TextSendMessage(text=str(events))
                     )
                 except LineBotApiError as e:
                     print(e.status_code)
