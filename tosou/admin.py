6cf82565e0724ab26716060fba441212321dbd52
shion-boss <sss.tl.ges.sss@gmail.com>
2020-06-14 20:55:06 +0900

    Update admin.py

--- tosou/admin.py
+++ tosou/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import customer_voice_model,user_meta,qa_model,catalog_model,message_table_model,message_user_model,account_meta
+from .models import customer_voice_model,user_meta,qa_model,catalog_model,message_table_model,message_user_model,account_meta,code_model
 # Register your models here.
 
 admin.site.register(customer_voice_model)
@@ -9,3 +9,4 @@ admin.site.register(catalog_model)
 admin.site.register(message_table_model)
 admin.site.register(message_user_model)
 admin.site.register(account_meta)
+admin.site.register(code_model)
