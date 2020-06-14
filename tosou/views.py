0eecea6eef9a2e50656b2c9846c4d154641952f4
shion-boss <sss.tl.ges.sss@gmail.com>
2020-06-14 12:20:06 +0900

    Update views.py

--- tosou/views.py
+++ tosou/views.py
@@ -39,6 +39,10 @@ json_list={
   ]
 }
 
+#line_user_id = events[0]['source']['userId']
+#if events[0]['type'] == 'message':
+#    text = request_json['events'][0]['message']['text']
+#    line_bot_api.push_message(line_user_id, TextSendMessage(text='Hello World!'))
 
 @csrf_exempt
 def callback_view(request):
@@ -70,15 +74,12 @@ def callback_view(request):
                         print(e.error.message)
                         print(e.error.details)
 
-            #if event['type'] == 'message':
-                #line_user_id = event['source']['userId']
-                #text = request_json['events'][0]['message']['text']
-                #line_bot_api.push_message(line_user_id, TextSendMessage(text='Hello World!'))
+            if event['type'] == 'message':
+                line_user_id = event['source']['userId']
+                text = request_json['events'][0]['message']['text']
+                line_bot_api.push_message(line_user_id, TextSendMessage(text='Hello World!'))
 
-        line_user_id = events[0]['source']['userId']
-        if events[0]['type'] == 'message':
-            text = request_json['events'][0]['message']['text']
-            line_bot_api.push_message(line_user_id, TextSendMessage(text='Hello World!'))
+
 
         return HttpResponse()
     else:
