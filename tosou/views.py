fab05f2f36c86d7ff8e3a6fa7471969925dc8e52
shion-boss <sss.tl.ges.sss@gmail.com>
2020-06-14 11:35:11 +0900

    Update views.py

--- tosou/views.py
+++ tosou/views.py
@@ -70,27 +70,6 @@ def callback_view(request):
                         print(e.error.message)
                         print(e.error.details)
 
-            #request_json = json.loads(body)
-            #events = request_json['events']
-            #events = json_list['events']
-            line_user_id = event['source']['userId']
-
-            # 友達追加時
-            if event['type'] == 'follow':
-                #profile = line_bot_api.get_profile(line_user_id)
-                #line_bot_api.push_message(line_user_id, TextSendMessage(text='Hello World!'))
-                pass
-
-            # アカウントがブロックされたとき
-            elif event['type'] == 'unfollow':
-                pass
-
-            # メッセージ受信時
-            elif event['type'] == 'message':
-                #text = event['message']['text']
-                #line_bot_api.push_message(line_user_id, TextSendMessage(text='Hello World!'))
-                pass
-        return HttpResponse('OK', status=200)
     else:
         return HttpResponseBadRequest()
 
