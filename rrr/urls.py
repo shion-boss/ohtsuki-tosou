e3199e0f7f68468f6f9bcb6c7920dc3c7ae63f60
shion-boss <sss.tl.ges.sss@gmail.com>
2020-06-18 00:41:59 +0900

    Update urls.py

--- rrr/urls.py
+++ rrr/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += staticfiles_urlpatterns()
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
